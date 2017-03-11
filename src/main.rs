825c691c8d4e6f44efd48563ebe71a1f0888ab31
Jack Valinsky <jvalinsk@u.rochester.edu>
2017-03-11 02:35:35 -0500

    example graphql

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 extern crate iron;
+extern crate mount;
+#[macro_use] extern crate juniper;
+
+use juniper::iron_handlers::{GraphQLHandler, GraphiQLHandler};
+use juniper::tests::model::Database;
+use juniper::EmptyMutation;
 use iron::prelude::*;
 use iron::status;
+use mount::Mount;
 
-fn hello_world(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello World!")))
+
+fn context_factory(_: &mut Request) -> Database {
+    Database::new()
 }
 
-
 fn main() {
-    let _server = Iron::new(hello_world).http("localhost:3000").unwrap();
+    let mut mount = Mount::new();
+    let graphql_endpoint = GraphQLHandler::new(
+        context_factory,
+        Database::new(),
+        EmptyMutation::<Database>::new(),
+    );
+    let graphiql_endpoint = GraphiQLHandler::new("/graphql");
+
+    mount.mount("/", graphiql_endpoint);
+    mount.mount("/graphql", graphql_endpoint);
+
     println!("Hello, world on 3000!");
+    Iron::new(mount).http("localhost:3000").unwrap();
 }
