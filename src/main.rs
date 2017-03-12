aaf96b442c631e3afd072fdf59ceab023290348c
Jack Valinsky <jvalinsk@u.rochester.edu>
2017-03-12 01:45:29 -0500

    example using serde to serve json derived from struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+#[macro_use]
+extern crate serde;
 extern crate iron;
 extern crate router;
 use iron::prelude::*;
 use router::Router;
 use iron::status;
 
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn index(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello world!"))) 
+    let point = Point { x: 1, y: 2 };
+    let serialized = serde_json::to_string(&point).unwrap();
+    Ok(Response::with((status::Ok,  serialized))) 
 }
 
 fn main() {
