93096ce0221d92bad17b512806656e0bcc1d9e1e
Jack Valinsky <jvalinsk@u.rochester.edu>
2017-03-12 07:10:05 -0400

    landing page

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ extern crate iron;
 extern crate router;
 extern crate staticfile;
 extern crate bodyparser;
-//extern crate mount;
+extern crate mount;
 
 use iron::prelude::*;
 use router::Router;
-//use mount::Mount;
+use mount::Mount;
 use iron::status;
 use staticfile::Static;
 use std::string::String;
@@ -19,39 +19,36 @@ use std::path::Path;
 mod models;
 use models::{ClientInput, Category, Categories};
 
-
-
-fn categorize(input: ClientInput) -> IronResult<Response> {
+fn categorize(input: ClientInput) -> String {
     let urls = input.urls;
+
     // send fake data
-     
     let cat = Category { name: String::from("tech"), urls: vec![String::from("news.ycombinator.com"), String::from("theverge.com"), String::from("arstechnica.com")] };
     let result = Categories { results: vec![cat] };
-    let serialized = serde_json::to_string(&result).unwrap();
-    Ok(Response::with((status::Ok,  serialized))) 
+    serde_json::to_string(&result).unwrap()
 }
 
 fn api(r: &mut Request) -> IronResult<Response> {
     let body = r.get::<bodyparser::Struct<ClientInput>>();
     match body {
-        Ok(Some(struct_body)) => categorize(struct_body),
+        Ok(Some(struct_body)) => Ok(Response::with((status::Ok, categorize(struct_body)))),
         Ok(None) =>  Ok(Response::with((status::BadRequest, "No data sent"))),
         Err(_) =>  Ok(Response::with((status::BadRequest, "Invalid input"))),
     }
 }
 
 fn main() {
-    
     let mut router = Router::new();
-    //let mut mount  = Mount::new();
-    let indexfile = Static::new(Path::new("static/index.html"));
+    let mut mount  = Mount::new();
+    let indexfile = Static::new(Path::new("public/index.html"));
+    let images = Static::new(Path::new("public/images"));
 
-    router.get("/api", api, "api");
-    router.get("/", indexfile, "index");
+    router.get("/", api, "api");
 
-    //mount.mount("/", router)
-    //     .mount("/", Static::new(Path::new("../static/index.html")));
+    mount.mount("/api", router)
+         .mount("/images/", images)
+         .mount("/", indexfile);
 
     println!("Hello, world on 3000!");
-    Iron::new(router).http("localhost:3000").unwrap();
+    Iron::new(mount).http("localhost:3000").unwrap();
 }
